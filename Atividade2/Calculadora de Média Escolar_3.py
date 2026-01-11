9d0ad6b43aa4b7a95efa24118ab67cc9141fa98a
Diego Vinicius <coelhodiego059@gmail.com>
2026-01-11 11:14:41 -0300

    Feat : Resolução do exercicio 3

--- Atividade2/Calculadora de Média Escolar_3.py
+++ Atividade2/Calculadora de Média Escolar_3.py
@@ -9,3 +9,26 @@ O programa deve calcular a média e exibir todas as notas e o resultado final, a
 para duas casas decimais.
 
 '''
+
+def media(lista_de_notas:list) ->float:
+    cont=0
+    soma=0
+    for nota in lista_de_notas:
+        cont+=1
+        soma+= nota
+    return soma/cont
+
+def lista_notas(*notas: float) -> list:
+    return list(notas)
+
+def main(*notas: float):
+    notas_lista = lista_notas(*notas)
+    resultado = media(notas_lista)
+    print(f"{resultado:.2f}")
+
+
+
+Nota_1 = 7.5
+Nota_2 = 8.0
+Nota_3 = 6.5
+main(Nota_1,Nota_2,Nota_3)
