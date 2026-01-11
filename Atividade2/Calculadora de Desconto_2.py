d5975d4c5a32de1de88a122ef9693004d2d304f0
Diego Vinicius <coelhodiego059@gmail.com>
2026-01-11 10:47:20 -0300

    Feat: resolução do exercicio 2

--- Atividade2/Calculadora de Desconto_2.py
+++ Atividade2/Calculadora de Desconto_2.py
@@ -8,3 +8,37 @@ Desenvolva um programa que calcula o desconto em uma loja. Use as seguintes info
 O programa deve calcular o valor do desconto e o preço final, exibindo todos os detalhes.
 
 '''
+class CalculePreco:
+
+    def __init__(self, nome_do_produto: str, preço_original: float, Porcentagem_de_desconto : float):
+        self.Nome_do_produto = nome_do_produto
+        self.Preço_original = preço_original
+        self.Porcentagem_de_desconto = Porcentagem_de_desconto
+
+    def Desconto(self) -> float:
+        return self.Preço_original*self.Porcentagem_de_desconto 
+
+    def PrecoOriginal(self) -> float:
+       return self.Preço_original - self.Desconto()
+
+    def retornar_json(self) -> dict:
+        return {
+            "produto": self.Nome_do_produto,
+            "preco_original": self.Preço_original,
+            "desconto": self.Desconto(),
+            "porcentagem_desconto": self.Porcentagem_de_desconto,
+            "preco_final": self.PrecoOriginal()
+        }
+
+def main():
+    produto = CalculePreco(
+        nome_do_produto="Camiseta",
+        preço_original=50.00,
+        Porcentagem_de_desconto=0.20
+    )
+
+    print(produto.retornar_json())
+
+
+if __name__ == "__main__":
+    main()
