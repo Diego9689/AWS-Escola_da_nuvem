7a9fb558da2b968426ae32b9c25c196729ebe07d
Diego Vinicius <coelhodiego059@gmail.com>
2026-01-10 22:44:56 -0300

    feat: Resoluçãodo desafio 1

--- Atividade2/Conversor de Moeda_1.py
+++ Atividade2/Conversor de Moeda_1.py
@@ -11,4 +11,18 @@ decimais.
 
 '''
 
+def convertDolar(valor:float)->  float:
+    return valor*5.20
 
+def convertEuro(valor:float)->  float:
+    return valor*6.15
+
+def exibir(valor:float):
+    print(f"O valor em Dolár é : {convertDolar(valor):.2f}")
+    print(f"O valor em Euro é : {convertEuro(valor):.2f}")
+
+Valor_em_reais =  100.00
+Taxa_do_dólar =  5.20
+Taxa_do_euro =  6.15
+
+exibir(Valor_em_reais)
