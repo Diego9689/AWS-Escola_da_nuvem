d235b76e5cf15e1435565d77e208c2e5fab30341
Diego Vinicius <coelhodiego059@gmail.com>
2026-01-11 12:05:49 -0300

    Feat : Resolução do desafio 4

--- Atividade2/Calculadora de Consumo de Combustível_4.py
+++ Atividade2/Calculadora de Consumo de Combustível_4.py
@@ -9,3 +9,25 @@ O programa deve calcular o consumo médio (km/l) e exibir todos os dados da viag
 o resultado final arredondado para duas casas decimais
 
 '''
+class Calcular_Consumo():
+    def __init__(self,distancia:float,combustivel:float):
+        self.Distância_percorrida = distancia
+        self.Combustível_gasto = combustivel
+    
+    def consumo_médio(self) -> float:
+        return self.Distância_percorrida/self.Combustível_gasto
+    
+    def Retornar_Json(self) -> str:
+        return f"Consumo médio : {self.consumo_médio():.2f} KM/L\nDistância_percorrida : {self.Distância_percorrida} KM\nCombustível_gasto : {self.Combustível_gasto} L"
+
+    def Main(self):
+        print(self.Retornar_Json())
+
+
+def chamarClasse():
+    consumo = Calcular_Consumo(distancia=300, combustivel=25)
+    consumo.Main()
+
+if __name__ == "__main__":
+    chamarClasse()
+ 
