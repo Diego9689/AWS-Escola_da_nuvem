e263c26f2707fc7c971afc9931e07230e7944202
Diego Vinicius <coelhodiego059@gmail.com>
2026-01-18 06:20:14 -0300

    Feat : adicionando desafios da atividade 6

---
+++ Atividade_6/Gerador_de_senhas_1.py
@@ -0,0 +1,5 @@
+'''
+1 - Crie um programa que gere senhas aleatórias com letras, números e símbolos e que 
+o usuário  também escolha o tamanho da senha  para criar senhas seguras 
+automaticamente.
+'''
